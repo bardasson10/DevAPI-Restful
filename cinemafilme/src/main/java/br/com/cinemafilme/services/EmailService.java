7affb19a739e6a17bdc857bd6207fb844682c0ba
Bardasson10 <gabriel123.bardasson@gmail.com>
2024-10-08 19:07:45 -0300

    configurações de email classe e app props configs

--- cinemafilme/src/main/java/br/com/cinemafilme/services/EmailService.java
+++ cinemafilme/src/main/java/br/com/cinemafilme/services/EmailService.java
@@ -3,7 +3,10 @@ package br.com.cinemafilme.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.stereotype.Component;
+
+import java.util.Properties;
 
 @Component
 public class EmailService {
@@ -13,5 +16,30 @@ public class EmailService {
 
     @Value("${spring.mail.username}")
     private String emailFrom;
+
+    @Value("${spring.mail.host}")
+    private String emailServerHost;
+
+    @Value("${spring.mail.port}")
+    private Integer emailServerPort;
+
+    @Value("${spring.mail.password}")
+    private String emailPassword;
+
+    public JavaMailSender javaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setUsername(emailFrom);
+        mailSender.setHost(emailServerHost);
+        mailSender.setPort(emailServerPort);
+        mailSender.setPassword(emailPassword);
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+
+        return mailSender;
+
+    };
+
 
 }
